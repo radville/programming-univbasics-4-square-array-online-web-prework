c8de1afc86fef661a1844bf76e674deba0826e67
Laura Radville <laura.radville@gmail.com>
2019-06-20 15:21:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   counter = 0
   
   while array[counter] do
-    array[counter] ** 2
-    puts array[counter]
-    return array[counter]
+    #array[counter] ** 2
+    return (array[counter] **2)
     counter += 1
   end
 end
